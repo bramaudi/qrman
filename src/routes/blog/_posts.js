18c5cc7e101c2a8c9b342bb1f193a18c9ce9849b
Brama Udi <bramaudi.al@gmail.com>
2020-06-11 21:04:58 +0700

    Fix minor bug

--- src/routes/blog/_posts.js
+++ src/routes/blog/_posts.js
@@ -1,20 +1,22 @@
 import all from '../../posts/**/*.md'
-import path from 'path'
+import { parse } from 'path'
 
 const 	_slugify_strip_re = /[^\w\s-]/g;
 const _slugify_hyphenate_re = /[-\s]+/g;
 function slugify(s) {
-	s = path.parse(s).name
+	s = parse(s).name
   s = s.replace(_slugify_strip_re, '').trim().toLowerCase();
   s = s.replace(_slugify_hyphenate_re, '-');
   return s;
 }
 
 const posts = []
+
 all.map(({ metadata, html, filename }) => {
 	// "title" & "date" are required fontmatter
 	// if not exist then mark it as a non-posts content
 	const { title, date } = metadata
+
 	if (title && date) {
 		const slug = slugify(filename)
 		const entry = {
