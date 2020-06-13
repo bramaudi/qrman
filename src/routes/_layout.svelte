6340aa59f40eac34b40568e0f36150b139b0f4ad
Brama Udi <bramaudi.al@gmail.com>
2020-06-13 19:49:40 +0700

    Improve dark mode reactivity

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -19,12 +19,54 @@
 		margin: 0 auto;
 		box-sizing: border-box;
 	}
-
 </style>
 
 <svelte:head>
 	{#if $theme === 'dark'}
-	<link rel="stylesheet" href="css/global-dark.css">
+	<style>
+	::-webkit-scrollbar-track {
+		background: #333333;
+	}
+	::-webkit-scrollbar-thumb {
+		background: #888888;
+	}
+	::-webkit-scrollbar-thumb:hover {
+		background: #696969;
+	}
+
+	body {
+		background: #242424;
+		color: #dedede;
+	}
+
+	.tags a {
+		background: #454545;
+		color: #ffffff;
+	}
+
+	table th, table td {
+		border: 1px solid #333333;
+	}
+	table tr {
+		background-color: #242424 !important;
+		border-top: 1px solid #dedede !important;
+	}
+	table tr:nth-child(2n) {
+		background-color: #292929 !important;
+	}
+
+	blockquote {
+		color: #797979 !important;
+		border-color: #555555 !important;
+	}
+
+	code {
+		background: #506475 !important;
+	}
+	pre > code {
+		background: inherit !important;
+	}
+	</style>
 	{/if}
 </svelte:head>
 
