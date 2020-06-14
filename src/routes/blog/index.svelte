a930b776b441084c2c7611bb7d1c052697feb553
Brama Udi <bramaudi.al@gmail.com>
2020-06-14 16:33:53 +0700

    Simple post search filter

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -8,13 +8,42 @@
 
 <script>
 	import PostList from '../../components/post-list.svelte'
-	export let posts;
+	export let posts
+	let keyword = ''
+
+	function filteredPosts(posts, keyword) {
+		return posts.filter(post => {
+			post = post['title'].toLowerCase()
+			return post.indexOf(keyword) > -1; 
+		});
+	}
 </script>
 
 <svelte:head>
 	<title>Blog</title>
 </svelte:head>
 
-<h1>All Posts</h1>
+<style>
+label {
+	display: block;
+	font-size: large;	
+}
+input {
+	margin: 0 0 10px;
+	padding: 8px;
+	background: #fff;
+	border: 1px solid #aaa;
+}
+</style>
 
-<PostList {posts} />
+<h1>All Posts</h1>
+<form>
+	<label for="search">Search</label>
+	<input type="text" id="search" bind:value={keyword} placeholder="Type post title">
+</form>
+
+{#if keyword === ''}
+	<PostList {posts} />
+{:else}
+	<PostList posts={filteredPosts(posts, keyword)} />
+{/if}
