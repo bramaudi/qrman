336b7b671bef56b1fe191b7032d39a602dc9c653
Brama Udi <bramaudi.al@gmail.com>
2020-07-05 04:13:26 +0700

    Bubblewrap init

---
+++ app/app/build/generated/source/buildConfig/release/id/my/bram/qrman/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package id.my.bram.qrman;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "id.my.bram.qrman";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1";
+}
